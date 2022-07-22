0083a83792595bd6ddbe2e00a2f2049baa6595ec
Kareem Dabbet <kareemda1@gmail.com>
2022-07-22 23:57:29 +0300

    improve meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,31 @@ export default defineNuxtConfig({
 
 	app: {
 		head: {
-			title: 'Gradient Text Generator',
+			title: 'CSS Gradient Text Generator',
 			link: [
+				{ rel: "icon", type: "image/x-icon", href: "favicon.ico?v=2" },
 				{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 				{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
 				{
 					rel: 'stylesheet',
 					href: 'https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap'
 				}
+			],
+			meta: [
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: "theme-color", content: "#42D392" },
+				{ name: 'title', content: 'CSS gradient text | free online gradient text generator' },
+				{ name: 'description', content: 'using CSS gradient text, you can generate gradient text using CSS, modify it (change colors - angle), then simply copy-paste it into your code' },
+				{ property: 'og:type', content: 'website' },
+				{ property: 'og:url', content: 'https://text-gradient.vercel.app' },
+				{ property: 'og:title', content: 'CSS gradient text | free online gradient text generator' },
+				{ property: 'og:description', content: 'using CSS gradient text, you can generate gradient text using CSS, modify it (change colors - angle), then simply copy-paste it into your code' },
+				{ property: 'og:image', content: 'https://text-gradient.vercel.app/meta-image.png' },
+				{ property: 'twitter:card', content: 'summary_large_image' },
+				{ property: 'twitter:url', content: 'https://text-gradient.vercel.app' },
+				{ property: 'twitter:title', content: 'CSS gradient text | free online gradient text generator' },
+				{ property: 'twitter:description', content: 'using CSS gradient text, you can generate gradient text using CSS, modify it (change colors - angle), then simply copy-paste it into your code' },
+				{ property: 'twitter:image', content: 'https://text-gradient.vercel.app/meta-image.png' }
 			]
 		}
 	}
